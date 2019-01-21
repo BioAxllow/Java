b9e26e04cb9aeb945b9c017bb9329f1e04777646
BioAxllow <bioaxllow@gmail.com>
2019-01-21 17:31:35 +0200

    First try

---
+++ Java/Hello World/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    int[] sveikiskaiciai={1,2,3,4};
+
+
+};
+
+public static int didziausias(int[] masyvas){
+
+        }
+
+
+public static void Test(){
+
+}
