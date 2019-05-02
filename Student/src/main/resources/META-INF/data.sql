d94bfe884aa8d0ae2d8198cf87d62176951d0156
BioAxllow <bioaxllow@gmail.com>
2019-05-02 23:06:39 +0300

    Student Update Main

--- Student/src/main/resources/META-INF/data.sql
+++ Student/src/main/resources/META-INF/data.sql
@@ -19,3 +19,48 @@ INSERT INTO mokytojai() VALUES ();
 -- FROM studentai AS s JOIN pazymiai AS p ON s.Id=p.studentas_id
 -- WHERE p.data='2018-08-12';
 
+-- SELECT * FROM studentai;
+-- SELECT * FROM pazymiai;
+--
+-- SELECT st.vardas, pz.pazymys FROM studentai st INNER JOIN pazymiai pz WHERE st.id=pz.studentas_id ORDER BY st.vardas;
+--
+-- -- Suskaičiuokite kiek iš viso lentelėje yra sudentų (įrašų):
+-- SELECT COUNT(*) FROM studentai;
+--
+-- -- Išveskite visus pažymius gautus 2018.08.12 dieną (sąrašas turi būti toks: studento vardas, pavardė, jo pažymių vidurkis).
+-- SELECT 	st.vardas AS 'studento vardas', st.pavarde AS 'pavardė', paz.pazymys AS 'gautas pažymys', paz.data
+-- FROM studentai st
+-- JOIN pazymiai paz
+-- ON st.id=paz.studentas_id
+-- WHERE paz.data='2018.08.12';
+--
+-- -- Išveskite visų studentų vidurkius (sąrašas turi būti toks: studento vardas, pavardė, jo pažymių vidurkius).
+-- SELECT st.vardas AS 'studento vardas', st.pavarde AS 'pavardė', CAST(AVG(paz.pazymys) AS DECIMAL(100,2)), paz.data AS vidurkis
+-- FROM studentai AS st
+-- JOIN pazymiai AS paz
+-- ON st.id=paz.studentas_id
+-- GROUP BY paz.studentas_id;
+--  -- GROUP BY st.id;
+--
+-- -- Išveskite visus studentus, kurių vidurkis didesnis nei 9.
+-- SELECT st.vardas, CAST(AVG(paz.pazymys) AS DECIMAL (3,2)) AS vidurkis
+-- FROM studentai AS st
+-- JOIN pazymiai AS paz
+-- ON st.id=paz.studentas_id
+-- GROUP BY paz.studentas_id
+-- HAVING vidurkis>9;
+--
+-- -- Išveskite tų studentų el. Pašto adresus kurie turi mažiau arba tik po vieną pažymį.
+-- SELECT st.el_pastas
+-- FROM studentai AS st
+-- WHERE (
+-- SELECT COUNT(paz.pazymys)
+-- FROM pazymiai paz
+-- WHERE st.id=paz.studentas_id
+-- GROUP BY paz.studentas_id)
+-- <=1;
+--
+-- SELECT * FROM studentai
+-- JOIN pazymiai ON studentai.id=pazymiai.studentas_id
+
+
